201a7b41a5e20c6581bfd11199eb5f953949772d
Joseph Young <jsyoung0891@gmail.com>
2019-06-10 16:14:40 +0100

    finished water bottle MVP

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    WaterBottle waterBottle = new WaterBottle(100);
+
+    @Test
+    public void canDrinkFromBottle(){
+        assertEquals(90, this.waterBottle.drink());
+    }
+
+    @Test
+    public void canEmptyWaterBottle(){
+        assertEquals(0, this.waterBottle.empty());
+    }
+
+    @Test
+    public void canFillWaterBottle(){
+        assertEquals(100, this.waterBottle.fill());
+    }
 }
